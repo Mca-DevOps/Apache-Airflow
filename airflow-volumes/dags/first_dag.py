ec0d7da5d8c19d36be516ecaf4819a9144bd6ec2
adotri-frimpong <goldmalcom43@gmail.com>
2026-01-06 12:57:30 +0100

    [FEAT] Update of the BashOperators DAG: first_dag.py

--- airflow-volumes/dags/first_dag.py
+++ airflow-volumes/dags/first_dag.py
@@ -2,15 +2,31 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+defaultArgs = {
+    "owner": "MCA",
+    "retries": 5
+}
+
 with DAG(
     dag_id="FirstDAG",
+    default_args=defaultArgs,
     description="The first DAG we created in 2026 !",
-    start_date=datetime(2026, 1, 3),
-    schedule="* * * * *"
+    start_date=datetime(2026, 1, 6),
+    schedule="*/30 * * * *"
 ) as dag:
     task1 = BashOperator(
         task_id = "first_bashOp_task",
-        bash_command = "echo hello world !"
+        bash_command = "echo i am task 1 !"
+    )
+
+    task2 = BashOperator(
+        task_id = "second_bashOp_task",
+        bash_command = "echo i am task 2 !"
+    )
+
+    task3 = BashOperator(
+        task_id = "third_bashOp_task",
+        bash_command = "echo i am task 3 !"
     )
     
-    task1
+    task1 >> [task2, task3]
