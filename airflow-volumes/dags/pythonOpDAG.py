a457708cac395958cf5943c1fc84896066bc8c96
adotri-frimpong <goldmalcom43@gmail.com>
2026-01-06 15:46:00 +0100

    [FEAT] Update of pythonOpDAG.py file

--- airflow-volumes/dags/pythonOpDAG.py
+++ airflow-volumes/dags/pythonOpDAG.py
@@ -5,19 +5,28 @@ from datetime import datetime
 
 # !!!! Next step: define a get_age() function which will deliver an age and just use the task_instance variable in the greet function in order to print fn, ln and age
 
-
-greet:Callable[[str, int],None] = lambda age, task_instance: print(f"Hello World ! My name is {task_instance.xcom_pull(task_ids='Get_name_task', key="first_name")} \
-                                                                    {task_instance.xcom_pull(task_ids='Get_name_task', key="last_name")} and I am {age} year(s) old .") \
+# greet which pull informations from get_age and get_name functions to greet the user
+greet:Callable[[airflow.models.taskinstance.TaskInstance],None] = lambda task_instance: print(f"Hello World ! My name is {task_instance.xcom_pull(task_ids='Get_name_task', key="first_name")} \
+                                                                    {task_instance.xcom_pull(task_ids='Get_name_task', key="last_name")} and I am {task_instance.xcom_pull(task_ids='Get_age_task', key="age")} year(s) old .") \
                                                                     if type(task_instance.xcom_pull(task_ids='Get_name_task', key="first_name"))==str and \
                                                                         type(task_instance.xcom_pull(task_ids='Get_name_task', key="last_name"))==str and \
-                                                                            type(age)==int else None
+                                                                            type(task_instance.xcom_pull(task_ids='Get_age_task', key="age"))==int else print("Incorrect variables type")
 
+
+# get_name function which push a first_name and a last_name in its XCom
 def get_name(task_instance:airflow.models.taskinstance.TaskInstance) -> None: task_instance.xcom_push(key="first_name", value="Jerry"); \
                                                                                 task_instance.xcom_push(key="last_name", value="Fridmann")
+
+
+# get_age function which push an age in its XCom
+get_age:Callable[[airflow.models.taskinstance.TaskInstance], None] = lambda task_instance: task_instance.xcom_push(key="age", value=24)
+
+
 defaultArgs = {
     "owner": "MCA",
     "retries": 5
 }
+
 
 with airflow.DAG(
     dag_id="PythonOp_DAG",
@@ -27,9 +36,8 @@ with airflow.DAG(
     schedule="*/30 * * * *"
 ) as dag:
     task1 = PythonOperator(
-        task_id = "first_PythonOp_task",
-        python_callable = greet,
-        op_kwargs={"age": 54}
+        task_id = "Get_age_task",
+        python_callable = get_age
     )
 
     task2 = PythonOperator(
@@ -37,16 +45,9 @@ with airflow.DAG(
         python_callable = get_name
     )
 
-    task2 >> task1
+    task3 = PythonOperator(
+        task_id = "first_PythonOp_task",
+        python_callable = greet
+    )
 
-    # task2 = PythonOperator(
-    #     task_id = "second_bashOp_task",
-    #     bash_command = "echo i am task 2 !"
-    # )
-
-    # task3 = PythonOperator(
-    #     task_id = "third_bashOp_task",
-    #     bash_command = "echo i am task 3 !"
-    # )
-    
-    # task1 >> [task2, task3]
+    [task1, task2] >> task3
