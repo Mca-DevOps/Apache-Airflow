30c7936e37f73ffb0d636aa54b14357455bc3949
adotri-frimpong <goldmalcom43@gmail.com>
2026-01-07 12:08:25 +0100

    [FEAT] Creation of airflow-volumes/dags/pythonOpTaskflow.py file : Creation of workflows with Taskflow feature

---
+++ airflow-volumes/dags/pythonOpTaskflow.py
@@ -0,0 +1,37 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+defaultArgs = {
+    "owner": "MCA",
+    "retries": 5
+}
+
+@dag(
+    dag_id="PythonOp_DAG_Taskflow",
+    default_args=defaultArgs,
+    description="The python operator DAG we created with Taskflow !",
+    start_date=datetime(2026, 1, 7),
+    schedule="*/30 * * * *"
+)
+def pythonOp_dag_taskflow():
+
+    @task(multiple_outputs=True)
+    def get_name() -> dict[str, str]: 
+        return {"first_name": "Jerry", 
+         "last_name":"Fridmann"}
+
+    @task()
+    def get_age() -> int:
+        return 24
+
+
+    @task()
+    def greet(name:dict[str, str], age:int) -> None:
+        print(f"Hello World ! My name is {name["first_name"]} {name["last_name"]} and I am {age} year(s) old .") if type(age)==int and type(name["first_name"])==str \
+                and type(name["last_name"])==str else print("Incorrect variables type")
+        
+    name = get_name(); age = get_age()
+    greet(name, age)
+        
+
+pythonOp_dag_taskflow()
