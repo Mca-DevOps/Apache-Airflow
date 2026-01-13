429a91a1d500b0fa20ecb29fda548539cb808273
adotri-frimpong <goldmalcom43@gmail.com>
2026-01-13 12:46:14 +0100

    [FEAT] Update of Makefile : Update of airflow-volumes/dags/dag_postgres_hooks.py file : Using hooks to share data between Postgres and MinIO AIStor

--- airflow-volumes/dags/dag_postgres_hooks.py
+++ airflow-volumes/dags/dag_postgres_hooks.py
@@ -2,27 +2,40 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from psycopg2.extensions import connection
-import csv, logging, os
+import csv, logging, os, tempfile
 
 
-# 1/ Query data from postgres and retrieve them in a .txt file locally
-# 2/ dump the file into minIO bucket
-
-def postgres_to_s3() -> None:
-    hook:PostgresHook = PostgresHook(postgres_conn_id="airflow_postgres")
-    conn:connection = hook.get_conn()
+def postgres_to_s3(ds_nodash) -> None:
+    # 1/ Query data from postgres and retrieve them in a .txt file locally
+    pg_hook:PostgresHook = PostgresHook(postgres_conn_id="airflow_postgres")
+    conn:connection = pg_hook.get_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM public.orders WHERE date <= '20220501'")
-    with open("dags/get_orders.txt", "w") as text_file:
-        logging.info(f"Abs path: {os.path.abspath("get_orders.txt")}")
+    cursor.execute("SELECT * FROM public.orders WHERE date <'20220430' ORDER BY DATE ASC;")
+
+    with tempfile.NamedTemporaryFile(mode='w', suffix=f"{ds_nodash}") as text_file:
+    # with open(f"dags/get_orders_{ds_nodash}.txt", "w") as text_file:
         writer = csv.writer(text_file)
         writer.writerow([column[0] for column in cursor.description])
         writer.writerows(cursor)
-    cursor.close()
-    conn.close()
-    logging.info(f"File 'get_orders.txt' successfully written !")
+        text_file.flush()
+        cursor.close()
+        conn.close()
+        logging.info(f"File '{text_file.name}' successfully written !")
 
+        # 2/ dump the file into minIO bucket
+        s3_hook:S3Hook = S3Hook(aws_conn_id="airflow_minio")
+        if s3_hook.check_for_bucket("airflow"):
+            s3_hook.load_file(
+                filename=text_file.name,
+                key=f"s3-test/{ds_nodash}.txt",
+                bucket_name="airflow",
+                replace=True
+            )
+            logging.info(f"File '{text_file.name}' successfully uploaded to MinIO S3 bucket 'airflow' !")
+        else:
+            logging.error("The bucket 'airflow' does not exist in MinIO !")
 
 
 
@@ -32,10 +45,10 @@ defaultArgs = {
 }
 
 with DAG(
-    dag_id="DAG_PostgresHooks",
+    dag_id="DAG_PostgresHooks_v02",
     default_args=defaultArgs,
     description="Introduction to Airflow Hooks !",
-    start_date=datetime(2026, 1, 12),
+    start_date=datetime(2022, 4, 30),
     schedule="@once",
     catchup=False
 ) as dag:
